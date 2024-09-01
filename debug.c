d19f440094148b69ad8c80f2bbb6ef8026e516a1
Tryfon Emmanouilidis <t.emmanouilidis@protonmail.com>
2024-09-01 10:04:07 +0300

    Continued with scanning, compiling to bytecode, and executing bytecode for simple binary instructions

--- debug.c
+++ debug.c
@@ -2,7 +2,7 @@
 #include "debug.h"
 #include "value.h"
 
-void disassembleChunk(Chunk* chunk, const char* name) {
+void disassembleChunk(Chunk *chunk, const char *name) {
     printf("== %s ==\n", name);
 
     for (int offset = 0; offset < chunk->count;) {
@@ -10,12 +10,12 @@ void disassembleChunk(Chunk* chunk, const char* name) {
     }
 }
 
-static int simpleInstruction(const char* name, int offset) {
+static int simpleInstruction(const char *name, int offset) {
     printf("%s\n", name);
     return offset + 1;
 }
 
-static int constantInstruction(const char* name, Chunk* chunk, int offset) {
+static int constantInstruction(const char *name, Chunk *chunk, int offset) {
     uint8_t constant = chunk->code[offset + 1];
     printf("%-16s %d '", name, constant);
     printValue(chunk->constants.values[constant]);
@@ -23,7 +23,7 @@ static int constantInstruction(const char* name, Chunk* chunk, int offset) {
     return offset + 2;
 }
 
-int disassembleInstruction(Chunk* chunk, int offset) {
+int disassembleInstruction(Chunk *chunk, int offset) {
     printf("%04d ", offset);
     if (offset > 0 && chunk->lines[offset] == chunk->lines[offset - 1]) {
         printf("   | ");
@@ -31,9 +31,21 @@ int disassembleInstruction(Chunk* chunk, int offset) {
         printf("%4d ", chunk->lines[offset]);
     }
     uint8_t instruction = chunk->code[offset];
-    switch(instruction) {
+    switch (instruction) {
         case OP_CONSTANT:
             return constantInstruction("OP_CONSTANT", chunk, offset);
+        case OP_NIL:
+            return simpleInstruction("OP_NIL", offset);
+        case OP_TRUE:
+            return simpleInstruction("OP_TRUE", offset);
+        case OP_FALSE:
+            return simpleInstruction("OP_FALSE", offset);
+        case OP_EQUAL:
+            return simpleInstruction("OP_EQUAL", offset);
+        case OP_GREATER:
+            return simpleInstruction("OP_GREATER", offset);
+        case OP_LESS:
+            return simpleInstruction("OP_LESS", offset);
         case OP_ADD:
             return simpleInstruction("OP_ADD", offset);
         case OP_SUBTRACT:
@@ -42,6 +54,8 @@ int disassembleInstruction(Chunk* chunk, int offset) {
             return simpleInstruction("OP_MULTIPLY", offset);
         case OP_DIVIDE:
             return simpleInstruction("OP_DIVIDE", offset);
+        case OP_NOT:
+            return simpleInstruction("OP_NOT", offset);
         case OP_NEGATE:
             return simpleInstruction("OP_NEGATE", offset);
         case OP_RETURN:
@@ -51,4 +65,3 @@ int disassembleInstruction(Chunk* chunk, int offset) {
             return offset + 1;
     }
 }
-
